62cd79ed440c7e096f69c96ddf025c038bf5273f
Bitello <bitello@tce.pr.gov.br>
2022-10-18 21:34:05 -0300

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: "#2fa",
+  },
+});
+</script>
+
+<template>
+  <button :style="{ backgroundColor: color }">
+    <!-- Dynamic Content -->
+    <slot />
+  </button>
+</template>
